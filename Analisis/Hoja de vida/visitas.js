df5afe342e08970c1da5efb3f45fa5a367b9e99f
veronica jazmin quintero valencia <quinteroveronica954@gmail.com>
2018-04-18 13:35:47 -0500

    currículum
    exercise, javascript, html5, css, responsive design

---
+++ Analisis/Hoja de vida/visitas.js
@@ -0,0 +1,8 @@
+$(function () {
+    // si variable no existe se crea (primera visita)
+    localStorage.cuenta = (localStorage.cuenta || 0);
+
+    localStorage.cuenta++; // incrementamos cuenta de visitas
+
+    $('#cuenta').html(localStorage.cuenta);
+});
